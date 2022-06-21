73f6e8090dd98cc41adff7c17e1c49dd9e441a8a
Liam Roche <roche.liamp@gmail.com>
2022-06-21 13:19:53 -0400

    added edit account component

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,14 +1,27 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import AuthContext from '../context/AuthContext'
 import { Link, Navigate } from 'react-router-dom'
 import './Nav.css'
 
 function Nav() {
+    let { user, logoutUser} = useContext(AuthContext)
+    const [navigate, setNavigate] = useState(false)
     return (
         <nav className="Nav">
             <h2>This is the nav component</h2>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Create Account</Link>
+            {!user?
+                <>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Create Account</Link>
+                </>
+                :
+                // NOTE - LOGGED IN USER ROUTES GO HERE
+                <>
+                    <a href="" onClick={()=>{logoutUser(); setNavigate(true)}}>Logout</a>
+                </>
+            }
+
+
         </nav>
     )
 }
