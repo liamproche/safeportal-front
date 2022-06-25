041b35650cedd6eda2f6620fac78cf90081bd3cd
Liam Roche <roche.liamp@gmail.com>
2022-06-25 15:41:04 -0400

    added buttons to post component

--- src/PrivateComponents/CommunityFeed/Posts/IndividualPost/IndividualPost.jsx
+++ src/PrivateComponents/CommunityFeed/Posts/IndividualPost/IndividualPost.jsx
@@ -3,7 +3,17 @@ import './IndividualPost.css'
 function IndividualPost(props){
     return (
         <div className="IndividualPost">
-            <h3>{props.user}</h3>
+            <p>Username?</p>
+            <p>Date</p>
+            <p>{props.content}</p>
+            <div className="controller-container">
+                <button>Like</button>
+                <button>Comment</button>
+                <button>Share</button>
+                <button>Send</button>
+            </div>
+
+        {/* THIS IS THE OVERALL COMPOENET DIV     */}
         </div>
     )}
   
