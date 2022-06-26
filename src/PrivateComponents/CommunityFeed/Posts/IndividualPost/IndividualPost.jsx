1b574b99812476d83ddbb5bd9d34079cda0c16b1
Liam Roche <roche.liamp@gmail.com>
2022-06-26 00:19:57 -0400

    deleted console.log

--- src/PrivateComponents/CommunityFeed/Posts/IndividualPost/IndividualPost.jsx
+++ src/PrivateComponents/CommunityFeed/Posts/IndividualPost/IndividualPost.jsx
@@ -3,7 +3,6 @@ import './IndividualPost.css'
 function IndividualPost(props){
     const formatDate = (dateString) =>{
         let tempDate = [...dateString]
-        console.log(tempDate)
         let day = tempDate.splice(8, 2).join('')
         let month = tempDate.splice(5, 2).join('')
         let year = tempDate.splice(0, 4).join('')
