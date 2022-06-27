d51ab51916e0341f01bcf6e8f71d316856fcf2ec
Liam Roche <roche.liamp@gmail.com>
2022-06-27 11:51:27 -0400

    added report instructions component

--- src/PrivateComponents/Reports/Reports.jsx
+++ src/PrivateComponents/Reports/Reports.jsx
@@ -1,11 +1,15 @@
 import ReportForm from './ReportForm/ReportForm'
+import ReportInstructions from './ReportInstructions/ReportInstructions'
 import Nav from "../Nav/Nav"
 import "./Reports.css"
 
 function Reports() {
     return <div className="Reports">
         <Nav/>
-        <ReportForm/>
+        <section className="reports-container">
+            <ReportInstructions/>
+            <ReportForm/>
+        </section>
     </div>
 }
 
