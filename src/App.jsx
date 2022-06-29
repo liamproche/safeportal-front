1fa55ab505dffa2d1bfde3c1c21464dd36592049
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-06-29 13:27:56 -0400

    tweaks to refresh token fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
-import './App.css';
-import EditAccount from './PrivateComponents/EditAccount/EditAccount';
-import PrivateComponents from './PrivateComponents/PrivateComponents';
-import Profile from './PrivateComponents/Profile/Profile';
+import "./App.css";
+import EditAccount from "./PrivateComponents/EditAccount/EditAccount";
+import PrivateComponents from "./PrivateComponents/PrivateComponents";
+import Profile from "./PrivateComponents/Profile/Profile";
 import Groups from "./PrivateComponents/Groups/Groups";
-import Testimonials from './PrivateComponents/Testimonials/Testimonials';
-import Reports from './PrivateComponents/Reports/Reports';
-import { AuthProvider } from './context/AuthContext'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-
+import Testimonials from "./PrivateComponents/Testimonials/Testimonials";
+import Reports from "./PrivateComponents/Reports/Reports";
+import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 
 function App() {
+  // const getCSRFToken = async () => {
+  //   const response = await fetch("http://localhost:3001/");
+  //   console.log(response);
+  // };
+
+  // useEffect(() => {
+  //   getCSRFToken();
+  // }, []);
+
   return (
     <div className="App">
       <Router>
         <AuthProvider>
           <Routes>
-            <Route path="/" element={<PrivateComponents/>} exact/>
-            <Route path="/profile" element={<Profile/>} exact/>
-            <Route path="/groups" element={<Groups/>} exact/>
-            <Route path="/account" element={<EditAccount/>} exact/>
-            <Route path="testimonials" element={<Testimonials/>} exact/>
-            <Route path="/report" element={<Reports/>} exact/>
-        </Routes>
-      </AuthProvider>
+            <Route path="/" element={<PrivateComponents />} exact />
+            <Route path="/profile" element={<Profile />} exact />
+            <Route path="/groups" element={<Groups />} exact />
+            <Route path="/account" element={<EditAccount />} exact />
+            <Route path="testimonials" element={<Testimonials />} exact />
+            <Route path="/report" element={<Reports />} exact />
+          </Routes>
+        </AuthProvider>
       </Router>
     </div>
   );
