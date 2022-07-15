75f460afc31604b42e5fa511a1fbc689251fc98d
Liam Roche <roche.liamp@gmail.com>
2022-07-15 13:12:02 -0400

    minor tweaks to styling

--- src/PublicComponents/Landing/About/About.jsx
+++ src/PublicComponents/Landing/About/About.jsx
@@ -3,10 +3,10 @@ import './About.css'
 function About() {
     return (
         <div className="About">
-            <h1>SafePlace International<br/> Virtual Community</h1>
+            <h1>SafePortal<br/> Virtual Community</h1>
             <div className="login-instructions">
                 <p>Welcome to the community where your safety comes first.</p>
-                <p className="second-paragraph">When signing in, use login information provided by SafePlace International Leader</p>
+                <p className="second-paragraph">When signing in, use login information provided by SafePortal Leader</p>
             </div>
         </div>
     );
